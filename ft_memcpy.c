3d476e0b3e978e0e13f6835a95b8a65c55d42c1f
Phanford <preston3050@gmail.com>
2019-10-08 16:07:20 -0700

    fixed weird bugs

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,7 @@
 /*   By: phanford <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 15:10:17 by phanford          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/10/07 23:01:08 by phanford         ###   ########.fr       */
-=======
-/*   Updated: 2019/10/07 17:24:14 by phanford         ###   ########.fr       */
->>>>>>> f261429005a3d7a523aebace7c554659aac86a2a
+/*   Updated: 2019/10/08 16:05:29 by phanford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +16,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char *srcptr;
 	unsigned char *dstptr;
-<<<<<<< HEAD
 
 	if (!src || !dst)
 		return (NULL);
 
-=======
-	
 	if (!dst || !src || !n)
 		return (dst);
->>>>>>> f261429005a3d7a523aebace7c554659aac86a2a
 	srcptr = (unsigned char *)src;
 	dstptr = dst;
 	while (n--)
