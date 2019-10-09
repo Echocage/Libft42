d715c4eb028392e01bbbba91f31d0219b9af6cfd
Phanford <preston3050@gmail.com>
2019-10-08 21:19:47 -0700

    last few

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,12 +6,12 @@
 /*   By: phanford <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 18:14:19 by phanford          #+#    #+#             */
-/*   Updated: 2019/10/08 21:13:53 by phanford         ###   ########.fr       */
+/*   Updated: 2019/10/08 21:18:26 by phanford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_putchar(char c)
+void	ft_putchar(int c)
 {
 	ft_putchar_fd(c, 1);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: phanford <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 11:37:53 by phanford          #+#    #+#             */
-/*   Updated: 2019/10/08 21:12:07 by phanford         ###   ########.fr       */
+/*   Updated: 2019/10/08 21:18:04 by phanford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,8 @@ void				ft_memdel(void **ap);
 char				*ft_strjoin(char const *s1, char const *s2);
 int					ft_strcmp(const char *s1, const char *s2);
 void				ft_strdel(char **as);
-void				ft_putchar_fd(char c, int fd);
-void				ft_putchar(char c);
+void				ft_putchar_fd(int c, int fd);
+void				ft_putchar(int c);
 void				ft_putendl(char const *s);
 char				*ft_itoa(int n);
 size_t				ft_strlen(const char *str);
